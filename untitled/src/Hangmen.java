b74aa1fc5386b770ec34304a0765801c37d34e29
Nakupenda25 <116508057+Nakupenda25@users.noreply.github.com>
2023-01-17 18:48:21 +0100

    included loads of new features

--- untitled/src/Hangmen.java
+++ untitled/src/Hangmen.java
@@ -7,6 +7,7 @@ public class Hangmen {
     public static void separatorLine(){
         System.out.printf("-".repeat(80) + "\n");
     }
+
     //returns a String randomly chosen from an ArrayList
     public static String getRandomWord (ArrayList<String> listOfWords){
         Random random = new Random();
@@ -14,9 +15,92 @@ public class Hangmen {
         return word;
     }
 
+    //method to that accepts an input and draws different sketches based on it
+    public static boolean drawHangmanLoss (int mistakes){
+
+        switch (mistakes) {
+            case 0:
+            return false;
+            case 1:
+                System.out.println("===");
+                return false;
+            case 2:
+                System.out.print(" |\n".repeat(4));
+                System.out.println("===");
+                return false;
+            case 3:
+                System.out.println("  ----");
+                System.out.print(" |\n".repeat(4));
+                System.out.println("===");
+                return false;
+            case 4:
+                System.out.println("  ----");
+                System.out.print(" |/\n");
+                System.out.print(" |\n".repeat(3));
+                System.out.println("===");
+                return false;
+            case 5:
+                System.out.println("  ----");
+                System.out.print(" |/   |\n");
+                System.out.printf(" |\n".repeat(3));
+                System.out.println("===");
+                return false;
+            case 6:
+                System.out.println("  ----");
+                System.out.printf(" |/   |\n");
+                System.out.print(" |    O\n");
+                System.out.printf(" |\n".repeat(2));
+                System.out.println("===");
+                return false;
+            case 7:
+                System.out.println("  ----");
+                System.out.printf(" |/   |\n");
+                System.out.print(" |    O\n");
+                System.out.print(" |    |\n");
+                System.out.print(" |\n");
+                System.out.println("===");
+                return false;
+            case 8:
+                System.out.println("  ----");
+                System.out.print(" |/   |\n");
+                System.out.print(" |    O\n");
+                System.out.print(" |    |\n");
+                System.out.print(" |   /\n");
+                System.out.println("===");
+                return false;
+            case 9:
+                System.out.println("  ----");
+                System.out.print(" |/   |\n");
+                System.out.print(" |    O\n");
+                System.out.print(" |    |\n");
+                System.out.print(" |   / \\\n");
+                System.out.println("===");
+                return false;
+            case 10:
+                System.out.println("  ----");
+                System.out.print(" |/   |\n");
+                System.out.print(" |    O\n");
+                System.out.print(" |   /|\n");
+                System.out.print(" |   / \\\n");
+                System.out.println("===");
+                return false;
+            case 11:
+                System.out.println("  ----");
+                System.out.print(" |/   |\n");
+                System.out.print(" |    O\n");
+                System.out.print(" |   /|\\\n");
+                System.out.print(" |   / \\\n");
+                System.out.println("===");
+                return true;
+
+            default:
+                throw new IllegalStateException("Unexpected value: " + mistakes);
+        }
+    }
+
     //adds every character of a String to an ArrayList for later use
     public static ArrayList<Character> wordIntoCharList (String word){
-        ArrayList<Character> wordAsList = new ArrayList<Character>();
+        ArrayList<Character> wordAsList = new ArrayList<>();
 
         for (char c : word.toCharArray()) {
             wordAsList.add(c);
@@ -25,21 +109,25 @@ public class Hangmen {
     }
 
     //checks if the path exists, and creates a new list containing all words within the given file
-    public static ArrayList<String> getWordList (String path) throws IOException {
+    public static ArrayList<String> getWordList (String path) {
         ArrayList words = new ArrayList();
         File file = new File(path);
 
-        if (file.exists() && file.length() != 0){
-            BufferedReader inBuffer = new BufferedReader(new FileReader(path));
-            String word;
+        try {
+            if (file.exists() && file.length() != 0){
+                BufferedReader inBuffer = new BufferedReader(new FileReader(path));
+                String word;
 
-            while ((word = inBuffer.readLine()) != null){
-            words.add(word);
-            }
-        }else if (file.length() == 0)
-            System.out.println("Error: Empty file!");
-        else
+                while ((word = inBuffer.readLine()) != null){
+                    words.add(word);
+                }
+            }else
+                System.out.println("Error: Empty file!");
+
+        }catch (IOException e){
             System.out.println("Error: Could not read file!");
+        }
+
 
         return words;
     }
@@ -50,6 +138,7 @@ public class Hangmen {
         //.matches uses regex to check if the String only follows a valid pattern
         for (String s : words) {
 
+            if (words.isEmpty() || s.matches(" "))
             if (!s.matches("[a-zA-Z]+")){
                 System.out.println("Error: Corrupt file!");
                 return false;
@@ -59,38 +148,95 @@ public class Hangmen {
     }
 
     //method for the character input of the user // resets if more than one character is provided
-    public static char charInput() {
-        char ch;
+    public static char charInput() throws InputMismatchException{
+        char ch = 0;
 
-        while (true) {
-            System.out.print("Next guess: ");
-            try {
-                String c = scn.nextLine();
-                if (c.length() == 1) {
-                    ch = c.charAt(0);
-                    if (Character.isLetter(ch))
-                        break;
-                }
-                throw new InputMismatchException("No valid input");
-            } catch (InputMismatchException e) {
-                System.out.println("Invalid Input!");
+        String c = scn.nextLine();
+        if (c.length() == 1) {
+            ch = c.charAt(0);
+            if (Character.isLetter(ch)){
+                return ch;
+            }
+            else{
+                ch = 0;
+                System.out.println("Invalid character!");
+                throw new InputMismatchException("Invalid Character");
             }
         }
-        return ch;
+        else {
+            System.out.println("Invalid input!");
+            throw new InputMismatchException("Invalid input!");
+        }
+
     }
 
     public static void main (String[]args){
-        ArrayList words = new ArrayList();
+        if (args.length == 0) {
+            System.out.println("Error: No file name given!");
+            return;
+        }
 
-        try {
-            words = getWordList(args[0]);
-        } catch (IOException e) {}
+        ArrayList<String> words = new ArrayList();
+        ArrayList<Character> wordPrint = new ArrayList();
+        ArrayList<Character> currentWord = new ArrayList<Character>();
+        HashSet userGuessedChars = new HashSet();
+        words = getWordList(args[0]);
+
+        int errorCounter = 0;
+        int roundCounter = 1;
+        int guessedLetters = 0;
 
         System.out.println("=".repeat(80));
         System.out.println("HANGMEN (" + words.size() + " Word(s))");
         separatorLine();
+        System.out.printf("Word #" + roundCounter +": \n\n");
+
+        for (int round = 1; round <= words.size(); round++) {
+            String temp;
+            temp = getRandomWord(words);
+            words.remove(temp);
+            currentWord = wordIntoCharList(temp);
+            for(int underscores = 1; underscores <= currentWord.size(); underscores++){
+                wordPrint.add('_');
+            }
+
+            System.out.println(currentWord);
+
+            while (true) {
+
+                if (errorCounter != 11) {
+                    try {
+                        for (int letternumber = 0; letternumber < currentWord.size(); letternumber++) {
+
+                            if (userGuessedChars.contains(currentWord.get(letternumber)) && wordPrint.get(letternumber) != currentWord.get(letternumber)) {
+                                wordPrint.set(letternumber, currentWord.get(letternumber));
+                                guessedLetters++;
+                            }
+                        }
+
+                        if (guessedLetters == currentWord.size())
+                            break;
+
+                        System.out.print("Word: ");
+                        for (char symbol : wordPrint) {
+                            System.out.print(symbol + " ");
+                        }
+
+                        System.out.println();
+                        System.out.println("Misses (" + errorCounter + "/11)");
+                        System.out.print("Next guess: ");
+
+                        char c = charInput();
+                        if (c != 0)
+                        userGuessedChars.add(c);
 
 
+                    } catch (InputMismatchException e) {}
+                }else
+                    break;
+            }
+            return;
+        }
 
 
     }
